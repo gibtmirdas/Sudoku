9202b93c8bba9e0bec868adecbb68d64491d700f
gibtmirdas <gibtmirdas@hotmail.fr>
2014-03-02 23:23:48 +0100

    number picker

--- src/com/example/Sudoku/SudokuPlay.java
+++ src/com/example/Sudoku/SudokuPlay.java
@@ -1,10 +1,13 @@
 package com.example.Sudoku;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.widget.NumberPicker;
 
 /**
  * Created by gibtmirdas on 02/03/14.
@@ -24,5 +27,39 @@ public class SudokuPlay extends Activity {
 		Log.d("Difficulty", "" + difficulty);
 
 		grid = new SudokuGrid(difficulty);
+		view.setGrid(grid);
+		view.setPlay(this);
+	}
+
+	public void launchDialogue(){
+		// 1. Instantiate an AlertDialog.Builder with its constructor
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		final NumberPicker picker = new NumberPicker(builder.getContext());
+		picker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+		picker.setMinValue(0);
+		picker.setMaxValue(9);
+
+		// 2. Chain together various setter methods to set the dialog characteristics
+		builder.setTitle("Select Value");
+		builder.setView(picker);
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener(){
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				setPickedValue(picker.getValue());
+			}
+		}).setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				setPickedValue(-1);
+			}
+		});
+
+		// 3. Get the AlertDialog from create()
+		AlertDialog dialog = builder.create();
+		dialog.show();
+	}
+
+	private void setPickedValue(int i){
+		view.setPickedValue(i);
 	}
 }
--- src/com/example/Sudoku/SudokuView.java
+++ src/com/example/Sudoku/SudokuView.java
@@ -1,13 +1,15 @@
 package com.example.Sudoku;
 
+import android.app.AlertDialog;
+import android.app.DialogFragment;
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
+import android.content.DialogInterface;
+import android.graphics.*;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by gibtmirdas on 02/03/14.
@@ -15,11 +17,13 @@ import android.view.View;
 public class SudokuView extends View {
 
 	private SudokuGrid grid;
+	private SudokuPlay play;
 	private int viewWidth, viewHeight;
 	private Color background;
-	private int xOffset=10, yOffset=10, delta=50;
+	private int xOffset = 10, yOffset = 10, delta = 50;
+	private Rect[][] rects = new Rect[9][9];
 
-	public SudokuView(Context context, AttributeSet attrs){
+	public SudokuView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
 
@@ -32,28 +36,26 @@ public class SudokuView extends View {
 
 		int strokeRayon = 5;
 
-		if(viewHeight > viewWidth)
+		if (viewHeight > viewWidth)
 			delta = viewWidth / 10;
 		else
 			delta = viewHeight / 10;
-		// vertical lines
-		for(int i=0; i<10; i++){
-			if(i%3==0){
+		// Draw grid
+		for (int i = 0; i < 10; i++) {
+			if (i % 3 == 0) {
 				dark.setStrokeWidth(10);
-			}else{
+			} else {
 				dark.setStrokeWidth(6);
 			}
-			canvas.drawLine(xOffset + delta*i,yOffset-strokeRayon, xOffset + delta*i,strokeRayon+ yOffset + 9*delta,dark );
-			canvas.drawLine(xOffset,yOffset + delta*i, xOffset + 9*delta,yOffset + delta*i,dark );
+			canvas.drawLine(xOffset + delta * i, yOffset - strokeRayon, xOffset + delta * i, strokeRayon + yOffset + 9 * delta, dark);
+			canvas.drawLine(xOffset, yOffset + delta * i, xOffset + 9 * delta, yOffset + delta * i, dark);
 		}
-
-		// horizontal lines
-//		for(int j=0; j<10; j++){
-//		}
-
-		// Draw board
-
-		// Exemple
+		// Place rects to identify which case the user clicked on
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				rects[i][j] = new Rect(xOffset + delta * j, yOffset + delta * i, xOffset + delta * (j + 1), yOffset + delta * (i + 1));
+			}
+		}
 	}
 
 	@Override
@@ -61,10 +63,34 @@ public class SudokuView extends View {
 		super.onSizeChanged(w, h, oldw, oldh);
 		viewWidth = w;
 		viewHeight = h;
-		Log.d("onSizeChanged", "Width:"+w+"--Height:"+h);
+		Log.d("onSizeChanged", "Width:" + w + "--Height:" + h);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+			int x = (int) event.getX(), y = (int) event.getY();
+
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					if (rects[i][j].contains(x, y)) {// Creating alert Dialog with one Button
+						play.launchDialogue();
+					}
+				}
+			}
+		}
+		return true;
 	}
 
 	public void setGrid(SudokuGrid grid) {
 		this.grid = grid;
+	}
+
+	public void setPlay(SudokuPlay play) {
+		this.play = play;
+	}
+
+	public void setPickedValue(int pickedValue) {
+		Log.d("Picked value", "" + pickedValue);
 	}
 }
